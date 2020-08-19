d4a35c399f65bd27ab9ff293d8117baff018c042
sreeraj <sreerajch657@gmail.com>
2020-08-19 13:20:37 +0530

    Create second largest of list.py

---
+++ practise questions/second largest of list.py
@@ -0,0 +1,10 @@
+#Python Program to Find the Second Largest Number in a List
+y=[]
+n=int(input("enter the limit of array : "))
+for i in range(0,n) :
+    x=int(input("enter the element to list : "))
+    y.append(x)
+
+y.sort()
+    
+print("the largest number among list is : %d "%(y[-2]))
