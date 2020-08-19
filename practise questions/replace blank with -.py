6102d374b4d88778f12b203bfa04c86246a5be53
sreeraj <sreerajch657@gmail.com>
2020-08-19 13:20:22 +0530

    Create replace blank with -.py

---
+++ practise questions/replace blank with -.py
@@ -0,0 +1,10 @@
+#Python Program to Take in a String and Replace Every Blank Space with Hyphen
+
+str_string=input("enter a string : ")
+
+
+for i in str_string :
+        str_string=str_string.replace(" ","-")
+        
+   
+print(str_string)
