fe85a58e7bd82b539abf7e8fefa254dad6896a8a
sreeraj <sreerajch657@gmail.com>
2020-08-19 13:21:18 +0530

    Create string length.py

---
+++ practise questions/string length.py
@@ -0,0 +1,10 @@
+#Python Program to Calculate the Length of a String Without Using a Library Function
+
+str_string=input("enter a string : ")
+
+count=0
+for i in str_string :
+        count=count+1
+        
+   
+print(count)
