824115196c1c862a91bf79944241da71bf5b9e03
sreeraj <sreerajch657@gmail.com>
2020-08-23 11:35:29 +0530

    Create swap numbers.py

---
+++ practise questions/swap numbers.py
@@ -0,0 +1,13 @@
+Python #Program to Exchange the Values of Two Numbers Without Using a Temporary Variable
+
+x=int(input("enter a number : "))
+y=int(input("enter second number : "))
+
+print(" before swapping : x=%d , y=%d " %(x,y))
+
+x,y=y,x
+
+print(" after swapping : x=%d , y=%d " %(x,y))
+
+
+
